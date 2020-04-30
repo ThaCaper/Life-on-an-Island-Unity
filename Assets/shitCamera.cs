e7c5620675c251103307d8ff850a243e28578511
andreas <andreas-cooper@hotmail.com>
2020-04-30 13:45:03 +0200

    shift Camera position by press C

---
+++ Assets/shitCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shitCamera : MonoBehaviour
+{
+
+
+    public Camera mainCamera;
+
+    public Camera CameraTwo;
+
+    public bool maincam = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.enabled = maincam;
+        CameraTwo.enabled = !maincam;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            maincam = !maincam;
+            mainCamera.enabled = maincam;
+            CameraTwo.enabled = !maincam;
+        }
+    }
+}
