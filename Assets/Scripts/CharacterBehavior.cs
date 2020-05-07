ab5528880b4c418eb930775aa2f692c25c51e48e
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2020-05-07 12:11:08 +0200

    Updating script for hitting

---
+++ Assets/Scripts/CharacterBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CharacterBehavior : MonoBehaviour
+{
+    public GameObject weaponIsOn;
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int randomAtk = Random.Range(1, 2);
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetFloat("Speed", 2.0f);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 1.0f);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Attack");
+            anim.SetInteger("RandomAttack", randomAtk);
+        }
+    }
+}
---
+++ Assets/Scripts/PickAxe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickAxe : MonoBehaviour, WeaponInterface
+{
+    // Start is called before the first frame update
+    public int dmg;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator WeaponAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
